6019aaad4375c6b6eabc9956ec8a1eb337e90196
SucaadSalaadcis <sucaadsalaad520@gmail.com>
2026-08-07 20:20:58 +0300

    png changed to PNG

--- src/Layouts/Footer/index.jsx
+++ src/Layouts/Footer/index.jsx
@@ -9,14 +9,14 @@
 //   const { t } = useTranslation();
 
 //   return (
-//     <footer className="footer relative">
+//     <footer className="relative footer">
     
 //       <div className="container">
 //         <div className="main-footer">
 //           <div className="row">
 //             <div className="col-md-3" style={{ paddingInlineStart: 0 }}>
-//               <div className="identity flex flex-col items-center md:translate-x-5">
-//                 <div className="brand mt-5">
+//               <div className="flex flex-col items-center identity md:translate-x-5">
+//                 <div className="mt-5 brand">
 //                   <Link href="/">
 //                     <Image
 //                       src={Logo}
@@ -139,19 +139,19 @@
 //                   </ul>
 //                 </div>
 //                 {/*  */}
-//                 <div className="col-md-4 text-center position-relative">
+//                 <div className="text-center col-md-4 position-relative">
                  
 
 //                   <h5 className="menu-title">
 //                     {t("menu:download_mobile_app")}
 //                   </h5>
 
-//                   <ul className="footer-list flex flex-col gap-1 items-center mt-4">
+//                   <ul className="flex flex-col items-center gap-1 mt-4 footer-list">
 //                     <li>
 //                       <Link
 //                         href="https://play.google.com/store/apps/details?id=YOUR_APP_ID"
 //                         target="_blank"
-//                         className="inline-block transform hover:scale-105 transition-transform"
+//                         className="inline-block transition-transform transform hover:scale-105"
 //                       >
 //                         <img
 //                           src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg"
@@ -166,7 +166,7 @@
 //                       <Link
 //                         href="https://apps.apple.com/app/idYOUR_APP_ID"
 //                         target="_blank"
-//                         className="inline-block transform hover:scale-105 transition-transform"
+//                         className="inline-block transition-transform transform hover:scale-105"
 //                       >
 //                         <img
 //                           src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg"
@@ -185,7 +185,7 @@
 //         {/*  */}
 //         <div className="copy-right">
 //           <div className="row align-items-center">
-//             <div className="col-md-12 text-center">
+//             <div className="text-center col-md-12">
 //               <p className="footer-text">
 //                 © 2026, <span className="focus">{t("common:site_name")}</span>{" "}
 //                 {t("common:all_rights_reserved_designed_by")}{" "}
@@ -204,7 +204,7 @@
 /////////
 import Link from "next/link";
 import Logo from "../../../public/images/general/logo.png";
-import MobileImage from "../../../public/images/general/mobile.png";
+import MobileImage from "../../../public/images/general/mobile.PNG";
 import { useTranslation } from "next-i18next";
 import Image from "next/image";
 
@@ -212,30 +212,30 @@ const Footer = () => {
   const { t } = useTranslation();
 
   return (
-    <footer className="footer relative pt-10 pb-6 border-t border-white/20 text-white">
-      <div className="container mx-auto px-4 max-w-6xl">
+    <footer className="relative pt-10 pb-6 text-white border-t footer border-white/20">
+      <div className="container max-w-6xl px-4 mx-auto">
         
         {/* Main Columns Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-12 gap-8 items-start mt-10">
+        <div className="grid items-start grid-cols-1 gap-8 mt-10 md:grid-cols-12">
           
           {/* Column 1: Logo & Socials */}
-          <div className="md:col-span-3 flex flex-col items-center md:items-start text-center md:text-left">
-            <Link href="/" className="mb-4 inline-block transition-transform hover:scale-105">
+          <div className="flex flex-col items-center text-center md:col-span-3 md:items-start md:text-left">
+            <Link href="/" className="inline-block mb-4 transition-transform hover:scale-105">
               <Image
                 src={Logo}
                 alt="Elfergany"
                 width={85}
                 height={85}
-                className="w-16 h-auto object-contain filter drop-shadow-md"
+                className="object-contain w-16 h-auto filter drop-shadow-md"
               />
             </Link>
 
-            <ul className="social-media flex items-center gap-2 mt-1">
+            <ul className="flex items-center gap-2 mt-1 social-media">
               <li>
                 <Link
                   href="https://www.facebook.com/marketelfergany"
                   target="_blank"
-                  className="w-9 h-9 rounded-lg bg-white/10 hover:bg-white/20 border border-white/20 flex items-center justify-center text-white transition-all hover:-translate-y-1"
+                  className="flex items-center justify-center text-white transition-all border rounded-lg w-9 h-9 bg-white/10 hover:bg-white/20 border-white/20 hover:-translate-y-1"
                 >
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
                     <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
@@ -246,7 +246,7 @@ const Footer = () => {
                 <Link
                   href="https://www.instagram.com/elferganymarket/"
                   target="_blank"
-                  className="w-9 h-9 rounded-lg bg-white/10 hover:bg-white/20 border border-white/20 flex items-center justify-center text-white transition-all hover:-translate-y-1"
+                  className="flex items-center justify-center text-white transition-all border rounded-lg w-9 h-9 bg-white/10 hover:bg-white/20 border-white/20 hover:-translate-y-1"
                 >
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
                     <rect x="2" y="2" width="20" height="20" rx="5" ry="5" />
@@ -259,7 +259,7 @@ const Footer = () => {
                 <Link
                   href="#"
                   target="_self"
-                  className="w-9 h-9 rounded-lg bg-white/10 hover:bg-white/20 border border-white/20 flex items-center justify-center text-white transition-all hover:-translate-y-1"
+                  className="flex items-center justify-center text-white transition-all border rounded-lg w-9 h-9 bg-white/10 hover:bg-white/20 border-white/20 hover:-translate-y-1"
                 >
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
                     <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
@@ -270,7 +270,7 @@ const Footer = () => {
                 <Link
                   href="#"
                   target="_self"
-                  className="w-9 h-9 rounded-lg bg-white/10 hover:bg-white/20 border border-white/20 flex items-center justify-center text-white transition-all hover:-translate-y-1"
+                  className="flex items-center justify-center text-white transition-all border rounded-lg w-9 h-9 bg-white/10 hover:bg-white/20 border-white/20 hover:-translate-y-1"
                 >
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
                     <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
@@ -283,7 +283,7 @@ const Footer = () => {
           </div>
 
           {/* Column 2: Company Links */}
-          <div className="md:col-span-3 text-center md:text-left">
+          <div className="text-center md:col-span-3 md:text-left">
             <h5 className="font-semibold text-base mb-3 text-white border-b border-white/20 pb-1.5 inline-block md:block">
               {t("menu:company")}
             </h5>
@@ -307,7 +307,7 @@ const Footer = () => {
           </div>
 
           {/* Column 3: Support Links */}
-          <div className="md:col-span-3 text-center md:text-left">
+          <div className="text-center md:col-span-3 md:text-left">
             <h5 className="font-semibold text-base mb-3 text-white border-b border-white/20 pb-1.5 inline-block md:block">
               {t("menu:help_support")}
             </h5>
@@ -331,8 +331,8 @@ const Footer = () => {
           </div>
 
           {/* Column 4: App Download Card (Vertical Display) */}
-          <div className="md:col-span-3 flex flex-col items-center text-center bg-white/10 p-4 rounded-2xl border border-white/20 backdrop-blur-sm">
-            <div className="w-36 h-36 relative mb-2">
+          <div className="flex flex-col items-center p-4 text-center border md:col-span-3 bg-white/10 rounded-2xl border-white/20 backdrop-blur-sm">
+            <div className="relative mb-2 w-36 h-36">
               <Image
                 src={MobileImage}
                 alt="App Showcase"
@@ -342,7 +342,7 @@ const Footer = () => {
               />
             </div>
 
-            <h5 className="font-semibold text-xs text-white mb-2">
+            <h5 className="mb-2 text-xs font-semibold text-white">
               {t("menu:download_mobile_app")}
             </h5>
 
@@ -350,25 +350,25 @@ const Footer = () => {
               <Link
                 href="https://play.google.com/store/apps/details?id=YOUR_APP_ID"
                 target="_blank"
-                className="hover:scale-105 transition-transform"
+                className="transition-transform hover:scale-105"
               >
                 <img
                   src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg"
                   alt="Play Store"
                   width={110}
-                  className="h-7 object-contain"
+                  className="object-contain h-7"
                 />
               </Link>
               <Link
                 href="https://apps.apple.com/app/idYOUR_APP_ID"
                 target="_blank"
-                className="hover:scale-105 transition-transform"
+                className="transition-transform hover:scale-105"
               >
                 <img
                   src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg"
                   alt="App Store"
                   width={110}
-                  className="h-7 object-contain"
+                  className="object-contain h-7"
                 />
               </Link>
             </div>
@@ -377,7 +377,7 @@ const Footer = () => {
         </div>
 
         {/* Bottom Copyright Bar */}
-        <div className="border-t border-white/15 pt-4 text-center text-lg text-white/80 pb-3">
+        <div className="pt-4 pb-3 text-lg text-center border-t border-white/15 text-white/80">
           © 2026, <span className="font-bold text-yellow-300">{t("common:site_name")}</span>.{" "}
           {t("common:all_rights_reserved_designed_by")}
         </div>
