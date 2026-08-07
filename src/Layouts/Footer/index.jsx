6d00f75759c2eb244e7d38e347a10d4ff8549a07
SucaadSalaadcis <sucaadsalaad520@gmail.com>
2026-08-07 20:28:33 +0300

    again the same error

--- src/Layouts/Footer/index.jsx
+++ src/Layouts/Footer/index.jsx
@@ -10,7 +10,7 @@
 
 //   return (
 //     <footer className="relative footer">
-    
+
 //       <div className="container">
 //         <div className="main-footer">
 //           <div className="row">
@@ -140,7 +140,6 @@
 //                 </div>
 //                 {/*  */}
 //                 <div className="text-center col-md-4 position-relative">
-                 
 
 //                   <h5 className="menu-title">
 //                     {t("menu:download_mobile_app")}
@@ -200,11 +199,9 @@
 
 // export default Footer;
 
-
 /////////
 import Link from "next/link";
 import Logo from "../../../public/images/general/logo.png";
-import MobileImage from "../../../public/images/general/mobile.PNG";
 import { useTranslation } from "next-i18next";
 import Image from "next/image";
 
@@ -214,13 +211,14 @@ const Footer = () => {
   return (
     <footer className="relative pt-10 pb-6 text-white border-t footer border-white/20">
       <div className="container max-w-6xl px-4 mx-auto">
-        
         {/* Main Columns Grid */}
         <div className="grid items-start grid-cols-1 gap-8 mt-10 md:grid-cols-12">
-          
           {/* Column 1: Logo & Socials */}
           <div className="flex flex-col items-center text-center md:col-span-3 md:items-start md:text-left">
-            <Link href="/" className="inline-block mb-4 transition-transform hover:scale-105">
+            <Link
+              href="/"
+              className="inline-block mb-4 transition-transform hover:scale-105"
+            >
               <Image
                 src={Logo}
                 alt="Elfergany"
@@ -237,7 +235,13 @@ const Footer = () => {
                   target="_blank"
                   className="flex items-center justify-center text-white transition-all border rounded-lg w-9 h-9 bg-white/10 hover:bg-white/20 border-white/20 hover:-translate-y-1"
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                  >
                     <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
                   </svg>
                 </Link>
@@ -248,7 +252,13 @@ const Footer = () => {
                   target="_blank"
                   className="flex items-center justify-center text-white transition-all border rounded-lg w-9 h-9 bg-white/10 hover:bg-white/20 border-white/20 hover:-translate-y-1"
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                  >
                     <rect x="2" y="2" width="20" height="20" rx="5" ry="5" />
                     <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
                     <line x1="17.5" y1="6.5" x2="17.51" y2="6.5" />
@@ -261,7 +271,13 @@ const Footer = () => {
                   target="_self"
                   className="flex items-center justify-center text-white transition-all border rounded-lg w-9 h-9 bg-white/10 hover:bg-white/20 border-white/20 hover:-translate-y-1"
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                  >
                     <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
                   </svg>
                 </Link>
@@ -272,7 +288,13 @@ const Footer = () => {
                   target="_self"
                   className="flex items-center justify-center text-white transition-all border rounded-lg w-9 h-9 bg-white/10 hover:bg-white/20 border-white/20 hover:-translate-y-1"
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                  >
                     <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
                     <rect x="2" y="9" width="4" height="12" />
                     <circle cx="4" cy="4" r="2" />
@@ -289,17 +311,26 @@ const Footer = () => {
             </h5>
             <ul className="space-y-2 text-sm text-white/90">
               <li>
-                <Link href="/about-us" className="hover:text-white hover:underline transition-colors block py-0.5">
+                <Link
+                  href="/about-us"
+                  className="hover:text-white hover:underline transition-colors block py-0.5"
+                >
                   {t("menu:about_us")}
                 </Link>
               </li>
               <li>
-                <Link href="/contact-us" className="hover:text-white hover:underline transition-colors block py-0.5">
+                <Link
+                  href="/contact-us"
+                  className="hover:text-white hover:underline transition-colors block py-0.5"
+                >
                   {t("menu:contact_us")}
                 </Link>
               </li>
               <li>
-                <Link href="/branches" className="hover:text-white hover:underline transition-colors block py-0.5">
+                <Link
+                  href="/branches"
+                  className="hover:text-white hover:underline transition-colors block py-0.5"
+                >
                   فروعنا
                 </Link>
               </li>
@@ -313,17 +344,26 @@ const Footer = () => {
             </h5>
             <ul className="space-y-2 text-sm text-white/90">
               <li>
-                <Link href="/terms-conditions" className="hover:text-white hover:underline transition-colors block py-0.5">
+                <Link
+                  href="/terms-conditions"
+                  className="hover:text-white hover:underline transition-colors block py-0.5"
+                >
                   {t("menu:terms_conditions")}
                 </Link>
               </li>
               <li>
-                <Link href="/privacy-policy" className="hover:text-white hover:underline transition-colors block py-0.5">
+                <Link
+                  href="/privacy-policy"
+                  className="hover:text-white hover:underline transition-colors block py-0.5"
+                >
                   {t("menu:privacy_policy")}
                 </Link>
               </li>
               <li>
-                <Link href="/return-policy" className="hover:text-white hover:underline transition-colors block py-0.5">
+                <Link
+                  href="/return-policy"
+                  className="hover:text-white hover:underline transition-colors block py-0.5"
+                >
                   {t("menu:return_policy")}
                 </Link>
               </li>
@@ -334,7 +374,7 @@ const Footer = () => {
           <div className="flex flex-col items-center p-4 text-center border md:col-span-3 bg-white/10 rounded-2xl border-white/20 backdrop-blur-sm">
             <div className="relative mb-2 w-36 h-36">
               <Image
-                src={MobileImage}
+                src="/images/general/mobile.png"
                 alt="App Showcase"
                 fill
                 sizes="64px"
@@ -373,15 +413,16 @@ const Footer = () => {
               </Link>
             </div>
           </div>
-
         </div>
 
         {/* Bottom Copyright Bar */}
         <div className="pt-4 pb-3 text-lg text-center border-t border-white/15 text-white/80">
-          © 2026, <span className="font-bold text-yellow-300">{t("common:site_name")}</span>.{" "}
-          {t("common:all_rights_reserved_designed_by")}
+          © 2026,{" "}
+          <span className="font-bold text-yellow-300">
+            {t("common:site_name")}
+          </span>
+          . {t("common:all_rights_reserved_designed_by")}
         </div>
-
       </div>
     </footer>
   );
